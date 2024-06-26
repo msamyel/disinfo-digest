c2289812763603e66de029e8abff6dfdcb27ebe8
Voslar Matej <matej@star-prince.com>
2024-06-26 15:10:08 +0200

    update database url

--- config.py
+++ config.py
@@ -7,5 +7,5 @@ dotenv.load_dotenv()
 class Config:
     DEBUG = os.getenv('DEBUG', False)
     API_SECRET = os.getenv('API_SECRET')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DB_URL', 'sqlite:///mydatabase.db')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///mydatabase.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
