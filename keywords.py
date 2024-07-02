572577d8cb02f19fc289f72041f8a836f59c685b
Voslar Matej <msamyel@gmail.com>
2024-07-02 19:18:36 +0200

    try to categorize keywords

--- keywords.py
+++ keywords.py
@@ -1,77 +1,101 @@
 # Dictionary pro vyhledávání slov v textu a jejich převod na kategorie.
 KEYWORDS_SEARCH_TABLE = {
+    #dezinformace
     'dezinform': 'dezinformace',
     'misinform': 'misinformace',
     'malinforma': 'malinformace',
     'hoax': 'hoax',
+    'konspir': 'konspirace',
+    'manipulace': 'manipulace',
+    'fake news': 'fake news',
+    'postfakt': 'doba postfaktická',
+    'pseudověd': 'pseudověda',
+
+    #strategická komunikace
+    'strategická komunik': 'strategická komunikace',
+    'stratcom': 'strategická komunikace',
+
+    #propaganda
     'propagand': 'propaganda',
-    'podvod': 'podvod',
+    'sociální inženýrství': 'sociální inženýrství',
+    'dragonbridge': 'dragonbridge',
+    'storm 1376': 'storm 1376',
+    'qanon': 'qanon',
+    'infowars': 'infoWars',
+    'false flag': 'false flag',
+    'hybridní hrozb': 'hybridní hrozba',
+    'informační válk': 'informační válka',
+    'narativ': 'narativ',
+    'mystifika': 'mystifikace',
+
+    #sociální sítě
     'twitter': 'x.com',
     'facebook': 'facebook',
-    'umělá inteligence': 'AI',
     'musk': 'Elon Musk',
     'tik tok': 'tik tok',
-    'konspir': 'konspirace',
+    'sociální sítě': 'sociální sítě',
+    'algoritm': 'algoritmus',
+    'gaslighting': 'gaslighting',
+
+    #AI
+    'umělá inteligence': 'AI',
     'deepfake': 'deepfake',
+    'AI': 'AI',
+
+    #Podvodné praktiky
+    'podvod': 'podvod',
+    'clickbait': 'clickbait',
     'botnet': 'botnet',
     'ddos': 'ddos',
     'troll': 'troll',
-    'sociální inženýrství': 'sociální inženýrství',
-    'clickbait': 'clickbait',
     'kyber': 'kyberbezpečnost',
-    'sociální sítě': 'sociální sítě',
-    'mediální gramotnost': 'mediální gramotnost',
-    'řetězový email': 'řetězáky',
-    'řetězové email': 'řetězáky',
-    'řetězák': 'řetězáky',
-    'svoboda slova': 'svoboda slova',
-    'fact-checking': 'fact-checking',
-    'cenzúr': 'cenzura',
-    'cenzur': 'cenzura',
-    'algoritm': 'algoritmus',
-    'overovanie faktov': 'fact-checking',
-    'ověřování faktů': 'fact-checking',
-    'AI': 'AI',
     'spamouflage': 'spamouflage',
     'spam': 'spam',
-    'dragonbridge': 'dragonbridge',
-    'storm 1376': 'storm 1376',
-    'prebunking': 'prebunking',
-    'debunking': 'debunking',
-    'fake news': 'fake news',
-    'qanon': 'qanon',
-    'infowars': 'infoWars',
     'doxx': 'doxxing',
-    'doxing': 'doxing',
-    'gaslighting': 'gaslighting',
-    'postfakt': 'doba postfaktická',
+    'doxing': 'doxxing',
     'brigading': 'brigading',
     'phishing': 'phishing',
     'scam': 'scam',
     'smishing': 'smishing',
-    'gerrymandering': 'gerrymandering',
     'vishing': 'vishing',
+    'hack': 'hacking',
+    'phreak': 'phreaking',
+    'sextortion': 'sextortion',
+    'gerrymandering': 'gerrymandering',
+    'hijacking': 'hijacking',
+
+    #Mediální gramotnost
+    'mediální gramotnost': 'mediální gramotnost',
+
+    #Řetězové zprávy
+    'řetězový email': 'řetězáky',
+    'řetězové email': 'řetězáky',
+    'řetězák': 'řetězáky',
+
+    #Svoboda slova
+    'svoboda slova': 'svoboda slova',
+    'cenzúr': 'cenzura',
+    'cenzur': 'cenzura',
+    'newspeak': 'newspeak',
+
+    #Fact-checking
+    'fact-checking': 'fact-checking',
+    'overovanie faktov': 'fact-checking',
+    'ověřování faktů': 'fact-checking',
+    'prebunking': 'prebunking',
+    'debunking': 'debunking',
+
+    #Radikalizace
     'manosféra': 'manosféra',
     'redpilling': 'redpilling',
+    'radikali': 'radikalismus',
     'krajní pravice': 'krajní pravice',
-    'false flag': 'false flag',
-    'hybridní hrozb': 'hybridní hrozba',
-    'hijacking': 'hijacking',
-    'informační válk': 'informační válka',
+    'overton': 'overtonovo okno',
+    'weaponiz': 'weaponizace',
+
+    #Kritické myšlení
     'konfirmačn': 'konfirmační bias',
     'kritické myšlení': 'kritické myšlení',
-    'mystifika': 'mystifikace',
-    'narativ': 'narativ',
-    'newspeak': 'newspeak',
-    'hack': 'hacking',
-    'phreak': 'phreak',
-    'radikali': 'radikalismus',
-    'pseudověd': 'pseudověda',
-    'overton': 'overtonovo okno',
-    'sextortion': 'sextortion',
-    'strategická komunik': 'strategická komunikace',
-    'stratcom': 'strategická komunikace',
-    'weaponiz': 'weaponizace'
 }
 
 # Dictionary pro převod kategorií na verzi bez diakritiky a obráceně.
