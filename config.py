d2db38dd79281b78a19e1edf32231f68ebe77e20
Voslar Matej <msamyel@gmail.com>
2024-07-07 17:24:11 +0200

    add sitemap endpoint

--- config.py
+++ config.py
@@ -6,6 +6,7 @@ dotenv.load_dotenv()
 
 class Config:
     DEBUG = os.getenv('DEBUG', False)
+    APP_HOST = os.getenv('APP_HOST', 'http://localhost:5000')
     API_SECRET = os.getenv('API_SECRET')
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///mydatabase.db').replace("postgres://", "postgresql://")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
