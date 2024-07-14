9376b7c66a2698f18b96902cebf6ca7488188202
Voslar Matej <msamyel@gmail.com>
2024-07-14 17:03:00 +0200

    fix boolean env var evaluation

--- config.py
+++ config.py
@@ -5,11 +5,11 @@ dotenv.load_dotenv()
 
 
 class Config:
-    DEBUG = os.getenv('DEBUG', 0) == 1
+    DEBUG = os.getenv('DEBUG', "false").lower() == "true"
     APP_HOST = os.getenv('APP_HOST', 'http://localhost:5000')
     API_SECRET = os.getenv('API_SECRET')
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///mydatabase.db').replace("postgres://", "postgresql://")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    BLUESKY_ENABLED = os.getenv('BLUESKY_ENABLED', 0) == 1
+    BLUESKY_ENABLED = os.getenv('BLUESKY_ENABLED', "false").lower() == "true"
     BLUESKY_HANDLE = os.getenv('BLUESKY_HANDLE')
     BLUESKY_APP_PASSWORD = os.getenv('BLUESKY_APP_PASSWORD')
