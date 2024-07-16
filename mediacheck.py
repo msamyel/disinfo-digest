d153816396b3a0a33aa006474ea536af4fa70502
Voslar Matej <msamyel@gmail.com>
2024-07-16 23:17:52 +0200

    change time span for checking articles

--- mediacheck.py
+++ mediacheck.py
@@ -186,7 +186,7 @@ if __name__ == "__main__":
 
     # Datumové rozmezí (datumy s časovou zónou UTC)
     now = datetime.now().astimezone(pytz.UTC)
-    start_date = now - timedelta(hours=24)
+    start_date = now - timedelta(hours=4)
     end_date = now
 
     with concurrent.futures.ThreadPoolExecutor(max_workers=NUM_THREADS) as executor:
